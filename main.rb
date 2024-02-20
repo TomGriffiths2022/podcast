effd15304688b23226338b15aaf170e42674e2c6
Tomos Griffiths <tomos.griffiths@dvla.gov.uk>
2024-02-20 11:58:01 +0000

    general cleanup

--- main.rb
+++ main.rb
@@ -1,12 +1,10 @@
 require_relative 'feed'
 
-ALLOWED_EPISODE_SELECTIONS = [1,2,3]
-
 ### SETUP METHODS ###
 def get_user_selection(user_choice, options_array)
   case user_choice.to_i
   when 1
-    puts Pastel.new.red("Playing #{options_array.first.title}....")
+    puts Pastel.new.on_green("Playing #{options_array.first.title}....")
     option_one = fork do
       # splitting the process into a fork to be able to isolate the signal for terminating
       Signal.trap("TERM") do
@@ -17,7 +15,7 @@ def get_user_selection(user_choice, options_array)
     Process.detach(option_one)
     user_listening_loop(option_one) 
   when 2
-    puts Pastel.new.red("Playing #{options_array[1].title}....")
+    puts Pastel.new.on_green("Playing #{options_array[1].title}....")
     option_two = fork do
       # splitting the process into a fork to be able to isolate the signal for terminating
       Signal.trap("TERM") do
@@ -27,7 +25,7 @@ def get_user_selection(user_choice, options_array)
     Process.detach(option_two)
     user_listening_loop(option_two) 
   when 3
-    puts Pastel.new.red("Playing #{options_array.last.title}....")
+    puts Pastel.new.on_green("Playing #{options_array.last.title}....")
     option_three = fork do
       # splitting the process into a fork to be able to isolate the signal for terminating
       Signal.trap("TERM") do
@@ -45,7 +43,7 @@ def user_listening_loop(chosen_option)
   # passing in which option to loop on
   loop do
     # put a prompt on screen to ask if the user wants to continue listening
-    puts Pastel.new.magenta.on_white("To exit playback, press E")
+    puts Pastel.new.on_red("To exit playback, press E")
     exit_playback = gets.upcase.chomp.to_s
     if exit_playback == 'E'
       # using the E key as the kill command to end the process
@@ -54,7 +52,7 @@ def user_listening_loop(chosen_option)
       break
     else
       # if user does not input E then display the message and return to the beginning of the loop
-      puts Pastel.new.magenta.on_white("Unknown option entered")
+      puts Pastel.new.on_blue("Unknown option entered")
     end
   end
 end
@@ -69,16 +67,17 @@ podcast = PodcastFeed.initialize_from_url('https://feeds.redcircle.com/c55cf02c-
 
 # initialise the creation of the PodcastFeed object based on the url given
 options = PodcastFeed.listening_options_to_user(podcast.recent_episodes)
-puts Pastel.new.magenta.on_white("Which recent episode would you like to listen too?: 1, 2 or 3")
+puts Pastel.new.white.on_magenta("Which recent episode would you like to listen too?: 1, 2 or 3 (or EXIT to quit)")
 
 loop do
   begin
     # determines the user's choice of which of the 3 latest episodes they would like to hear
-    user_selection = gets.chomp.to_i
+    user_selection = gets.chomp
+    break if user_selection == 'EXIT'
     # passing in the user's choice as well as the array of episode options to choose from
-    get_user_selection(user_selection, options)
+    get_user_selection(user_selection.to_i, options)
     break
   rescue ArgumentError => e
-    puts Pastel.new.magenta.on_white("#{e.message}")
+    puts Pastel.new.white.on_red("#{e.message}")
   end
 end
